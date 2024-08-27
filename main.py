50da83595d51569aa0e5a4d935d6f486b28d695a
juantreses <jandries@live.be>
2024-08-27 16:26:03 +0200

    Adds Character report

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ def main():
     book = get_book_text(book_path)
     word_count = count_words(book)
     chars_dict = get_chars_dict(book)
+    sorted_list = char_dict_to_sorted_list(chars_dict)
 
-    print(f"{chars_dict}")
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def get_book_text(file_path):
@@ -13,6 +23,16 @@ def get_book_text(file_path):
 
 def count_words(text):
     return len(text.split())
+
+def sort_on(d):
+    return d["num"]
+
+def char_dict_to_sorted_list(chars_dict):
+    sorted_list = []
+    for ch in chars_dict:
+        sorted_list.append({"char": ch, "num": chars_dict[ch]})
+    sorted_list.sort(key=sort_on, reverse=True)
+    return sorted_list
 
 def get_chars_dict(text):
     chars_dict = {}
