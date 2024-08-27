f0a6385380a24cc7778d6e92460c0ad9d8451afb
juantreses <jandries@live.be>
2024-08-27 16:22:01 +0200

    Add a chars dict method

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     book = get_book_text(book_path)
     word_count = count_words(book)
+    chars_dict = get_chars_dict(book)
 
-    print(f"The book has {word_count} words.")
+    print(f"{chars_dict}")
 
 
 def get_book_text(file_path):
@@ -12,5 +13,14 @@ def get_book_text(file_path):
 
 def count_words(text):
     return len(text.split())
+
+def get_chars_dict(text):
+    chars_dict = {}
+    for char in text.lower():
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    return chars_dict
 
 main()
